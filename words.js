8c3b81b858103b413fc9222e3c16b9073a44e1a6
Pierre-gruenais <Pierre.gruenais@oclcock.school>
2024-09-23 16:02:39 +0200

    modification clavier

--- words.js
+++ words.js
@@ -23,3 +23,97 @@ export const words = [
   "wagon",
   "zebre",
 ];
+<div class="grid grid-cols-5 gap-2 justify-center">
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  A
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  B
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  C
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  D
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  E
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  F
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  G
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  H
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  I
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  J
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  K
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  L
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  M
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  N
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  O
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  P
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  Q
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  R
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  S
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  T
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  U
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  V
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  W
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  X
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  Y
+</button>
+<button class="key bg-gray-300 hover:bg-gray-400 p-2 rounded">
+  Z
+</button>
+<button
+  class="key bg-gray-300 hover:bg-gray-400 p-2 rounded col-span-2"
+  id="enter-key"
+>
+  enter
+</button>
+<button
+  class="key bg-gray-300 hover:bg-gray-400 p-2 rounded col-span-2"
+  id="delete-key"
+>
+  delete
+</button>
+</div>
+</div>
+</div>
